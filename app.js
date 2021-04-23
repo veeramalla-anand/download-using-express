7b7679d2f051b7ee6fc57b8f089584aa845a5575
Ananda Reddy <71586050+veeramalla-anand@users.noreply.github.com>
2021-04-23 18:48:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var path = require('path');
+var app = module.exports = express();
+
+app.get('/', function(req, res){
+  res.send('<ul>'
+    + '<li>Download <a href="/files/amazing.txt">amazing.txt</a>.</li>'
+    + '<li>Download <a href="/files/missing.txt">missing.txt</a>.</li>'
+    + '<li>Download <a href="/files/images.jpg">beautiful boy</a>.</li>'
+    + '</ul>');
+});
+
+// /files/* is accessed via req.params[0]
+// but here we name it :file
+app.get('/files/:file(*)', function(req, res, next){
+  var filePath = path.join(__dirname, 'files', req.params.file);
+
+  res.download(filePath, function (err) {
+    if (!err) return; // file sent
+    if (err.status !== 404) return next(err); // non-404 error
+    // file for download not found
+    res.statusCode = 404;
+    res.send('Cant find that file, sorry!');
+  });
+});
+
+if (!module.parent) {
+  app.listen(5000);
+  console.log('Express started on port 5000');
+}
